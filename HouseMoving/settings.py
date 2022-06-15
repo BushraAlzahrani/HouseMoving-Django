f7bdf55e3f57856b696639855b51e15939e9231a
bushra <bushra.azbm@gmail.com>
2022-06-15 08:45:58 +0300

    heroku 2

--- HouseMoving/settings.py
+++ HouseMoving/settings.py
@@ -31,7 +31,7 @@ import os
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 DEBUG = os.environ.get('DJANGO_DEBUG', True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['housemoving.herokuapp.com','127.0.0.1']
 
 
 # Application definition
